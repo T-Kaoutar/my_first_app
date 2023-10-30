b42447795dd793f11581c176f5992c6436c66a21
kaoutar <kaoutar.ta17@gmail.com>
2023-10-31 00:40:02 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col items-center">
-        <h1>Welcome to my website</h1>
-        <h2>by Kaoutar</h2>
+      <div className="bg-slate-500">
+        <div className=" h-screen flex flex-col justify-center items-center">
+          <p className="text-4xl font-bold">Welcome to my website</p>
+          <p className=" ml-48 italic underline decoration-red-500 hover:text-slate-300">
+            by Kaoutar
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <p> tests</p>
+          <h1></h1>
+          <div></div>
+          <span></span>
+          <input type="text" />
+
+          <input type="checkbox" />
+          <label htmlFor=""> check Box</label>
+          <form action=""></form>
+          <pre className=""> Pre tag</pre>
+        </div>
       </div>
     </>
   );
