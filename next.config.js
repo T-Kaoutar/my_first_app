fd3c1eee45d178cd8627ae14c8c626ee261393fa
kaoutar <kaoutar.ta17@gmail.com>
2023-10-31 23:44:21 +0100

    export

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = { output: "export" };
 
 // module.exports = nextConfig;
 
-module.exports = {
-  output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "" : undefined,
-  experimental: {
-    appDir: true,
-    serverActions: true,
-  },
-};
+// module.exports = {
+//   output: "export",
+//   basePath: process.env.NODE_ENV === "production" ? "" : undefined,
+//   experimental: {
+//     appDir: true,
+//     serverActions: true,
+//   },
+// };
